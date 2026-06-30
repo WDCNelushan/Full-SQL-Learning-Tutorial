9efc8b1a1cd14a0bb89ae78878416c89128e1862
WDCNelushan <nelushanwickramarachchi2002@gmail.com>
2026-06-30 11:48:36 +0530

    Added Joins in SQL: Inner join, Left Outer Join, Right Outer Join, Self Join, Multiple Join

---
+++ Intermediate SQL Commands.sql
@@ -0,0 +1,73 @@
+select * 
+from parks_and_recreation.employee_demographics;
+
+select * 
+from parks_and_recreation.employee_salary;
+
+select * 
+from parks_and_recreation.parks_departments;
+
+-- Joins
+
+-- Inner Join
+
+select dem.employee_id, age, occupation
+from employee_demographics AS dem
+INNER JOIN employee_salary As sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT demo.employee_id, demo.first_name, demo.age, sal.salary
+FROM employee_demographics AS demo
+INNER JOIN employee_salary AS sal
+    ON demo.employee_id = sal.employee_id;
+    
+-- LEFT OUTER Join
+
+select *
+from employee_demographics AS dem
+LEFT JOIN employee_salary AS sal 
+	 ON dem.employee_id = sal.employee_id
+where sal.salary > 50000;
+
+SELECT sal.employee_id, sal.first_name, sal.salary, demo.age
+FROM employee_salary AS sal
+LEFT JOIN employee_demographics AS demo
+    ON sal.employee_id = demo.employee_id;
+    
+-- RIGHT OUTER Join
+
+select *
+from employee_demographics AS dem
+RIGHT JOIN employee_salary AS sal 
+	 ON dem.employee_id = sal.employee_id;
+     
+SELECT sal.employee_id, demo.first_name, sal.salary, demo.age
+FROM employee_demographics AS demo
+RIGHT JOIN employee_salary AS sal
+    ON demo.employee_id = sal.employee_id;
+    
+-- SELF Join
+
+select *
+from employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id + 1 = emp2.employee_id;
+
+SELECT emp1.first_name AS Employee, emp2.first_name AS Coworker, emp1.dept_id  -- people who work in the same department.
+FROM employee_salary emp1
+JOIN employee_salary emp2
+    ON emp1.dept_id = emp2.dept_id 
+    AND emp1.employee_id != emp2.employee_id;
+    
+-- Multiple Joins
+
+SELECT demo.first_name, demo.last_name, sal.salary, dept.department_name
+FROM employee_demographics AS demo
+JOIN employee_salary AS sal
+    ON demo.employee_id = sal.employee_id
+JOIN parks_departments AS dept
+    ON sal.dept_id = dept.department_id;
+
+
+     
+     
